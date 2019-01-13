fd8454f1c8a886446771b1130c4b6f6a1270c51f
ex_huangwc <872157571@qq.com>
2019-01-14 00:13:55 +0800

    Site updated: 2019-01-14 00:13:53

--- dist/music.js
+++ dist/music.js
@@ -4,22 +4,28 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "PDD洪荒之力",
-        artist: '徐梦圆',
-        url: 'http://up.mcyt.net/?down/39868.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+        name: "你一定要幸福",
+        artist: '简弘亦',
+        url: 'http://www.ytmp3.cn/down/51689.mp3',
+        cover: 'http://img.ytmp3.cn/image/52.jpg',
       },
       {
-        name: '9420',
-        artist: '麦小兜',
-        url: 'http://up.mcyt.net/?down/45967.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+        name: '一百万个可能(Live)',
+        artist: '摩登兄弟',
+        url: 'http://www.ytmp3.cn/down/52772.mp3',
+        cover: 'http://img.ytmp3.cn/image/53.jpg',
       },
       {
-        name: '风筝误',
-        artist: '刘珂矣',
-        url: 'http://up.mcyt.net/?down/46644.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+        name: 'The Rose',
+        artist: 'Westlife',
+        url: 'http://www.ytmp3.cn/down/56694.mp3',
+        cover: 'http://img.ytmp3.cn/image/51.jpg',
+      },
+	  {
+        name: 'In The Eyes',
+        artist: '江映东',
+        url: 'http://www.ytmp3.cn/down/53053.mp3',
+        cover: 'http://img.ytmp3.cn/image/10.jpg',
       }
     ]
 });
